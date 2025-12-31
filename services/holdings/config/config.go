49353e0f53d6604da3bf393bcc4ab1cd39793584
Levi Segal <levisegal@users.noreply.github.com>
2025-12-30 20:16:46 -0800

    Add configuration and version management for Monay holdings service, including environment variable parsing and default settings.

---
+++ services/holdings/config/config.go
@@ -0,0 +1,60 @@
+package config
+
+import (
+	"sync"
+
+	"dario.cat/mergo"
+	"github.com/caarlos0/env/v11"
+)
+
+var (
+	once   sync.Once
+	config *Config
+)
+
+func Load() (*Config, error) {
+	var err error
+	once.Do(func() {
+		config, err = NewConfig()
+	})
+	return config, err
+}
+
+func NewConfig() (*Config, error) {
+	conf := defaultConfig()
+	envConfig := &Config{}
+
+	err := env.ParseWithOptions(envConfig, env.Options{Prefix: "MONAY_HOLDINGS_"})
+	if err != nil {
+		return nil, err
+	}
+
+	err = mergo.Merge(conf, envConfig, mergo.WithOverride)
+	if err != nil {
+		return nil, err
+	}
+
+	return conf, nil
+}
+
+func defaultConfig() *Config {
+	return &Config{
+		ListenAddr:   ":8080",
+		LoggingLevel: "info",
+		Plaid: Plaid{
+			Env: "sandbox",
+		},
+	}
+}
+
+type Config struct {
+	ListenAddr   string `env:"LISTEN_ADDR"`
+	LoggingLevel string `env:"LOGGING_LEVEL"`
+	Plaid        Plaid  `envPrefix:"PLAID_"`
+}
+
+type Plaid struct {
+	ClientID string `env:"CLIENT_ID"`
+	Secret   string `env:"SECRET"`
+	Env      string `env:"ENV"`
+}
