996798deb6f6f0df6e20c5af0382a732e639514c
Levi Segal <levisegal@users.noreply.github.com>
2025-12-30 20:05:05 -0800

    Add main.go to implement the entry point for the holdings service, handling OS signals for graceful shutdown and executing command logic.

---
+++ services/holdings/cmd/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/levisegal/monay/services/holdings/cmd/cmd"
+)
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	defer func() {
+		signal.Stop(sigChan)
+		close(sigChan)
+	}()
+
+	go func() {
+		sig := <-sigChan
+		slog.InfoContext(ctx, "Received signal, initiating shutdown", "signal", sig.String())
+		cancel()
+	}()
+
+	if err := cmd.Execute(ctx); err != nil {
+		os.Exit(1)
+	}
+}
